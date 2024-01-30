3804284d4eb11df42935874f2f0a0e392a0d2de8
Nagy Zétény <nagyzeteny6@gmail.com>
2024-01-30 16:16:56 +0100

    fix handling non exisiting json file

--- src/lib/get_todos.rs
+++ src/lib/get_todos.rs
@@ -1,7 +1,7 @@
 use super::Todo;
 
 pub fn get_todos() -> Vec<Todo> {
-    let json = std::fs::read_to_string("todos.json").unwrap_or(String::new());
+    let json = std::fs::read_to_string("todos.json").unwrap_or(String::from("[]"));
     let todos = serde_json::from_str(&json).expect("Failed to parse json");
 
     return todos;
