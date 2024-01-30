5bade4e0f198268dd83656c8cc4bddc695f52d19
Nagy Zétény <nagyzeteny6@gmail.com>
2024-01-30 20:43:27 +0100

    better formatting

--- src/lib/select_action.rs
+++ src/lib/select_action.rs
@@ -3,23 +3,28 @@ use super::Todo;
 use dialoguer::{theme::ColorfulTheme, Select};
 
 pub enum Selection {
+    Ignore,
     Add,
     Exit,
     Todo(usize),
 }
 
 pub fn select_action(todos: &Vec<Todo>) -> Selection {
-    let mut select_from = todos
-        .iter()
-        .map(|todo| todo.get_formatted_title())
-        .collect::<Vec<_>>();
+    let mut select_from = Vec::with_capacity(todos.len() + 3);
+    select_from.push(String::new());
 
+    select_from.extend(todos.iter().map(|todo| todo.get_formatted_title()));
     select_from.extend(["Add".to_string(), "Exit".to_string()]);
 
     let selection = Select::with_theme(&ColorfulTheme::default())
         .items(&select_from)
+        .default(1)
         .interact()
         .expect("Failed to select todo");
+
+    if selection == 0 {
+        return Selection::Ignore;
+    }
 
     if selection == select_from.len() - 1 {
         return Selection::Exit;
@@ -29,5 +34,5 @@ pub fn select_action(todos: &Vec<Todo>) -> Selection {
         return Selection::Add;
     }
 
-    return Selection::Todo(selection);
+    return Selection::Todo(selection - 1);
 }
--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,15 @@ fn main() {
     let mut todos = get_todos();
 
     loop {
-        if todos.len() == 0 {
-            println!("\nYou have no todos! Done for now :)\n");
-        }
-
         todos.sort_by(|a, b| a.completed.cmp(&b.completed));
 
         match select_action(&todos) {
+            Selection::Ignore => (),
+            Selection::Add => add_todo(&mut todos),
             Selection::Exit => {
                 todos.save_to_file();
                 break;
             }
-            Selection::Add => add_todo(&mut todos),
             Selection::Todo(index) => handle_todo(&mut todos, index),
         }
     }
